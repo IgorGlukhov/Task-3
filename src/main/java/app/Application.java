9850d05e77c01890c932f2c12a27e1c72564d8fa
Igor Glukhov <igor5d@rambler.ru>
2024-11-23 12:09:38 +0400

    solution

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,12 +1,10 @@
 package app;
-
 import app.config.AppConfig;
 import app.model.AnimalsCage;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Application {
-
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
@@ -16,5 +14,4 @@ public class Application {
             bean.whatAnimalSay();
         }
     }
-
 }
